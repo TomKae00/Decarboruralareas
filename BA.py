226aaf744d04169858445c6fec2f1c2b8fb5670d
lugi <lukas9592@gmail.com>
2023-10-03 21:33:49 +0200

    lol

--- BA.py
+++ BA.py
@@ -15,7 +15,8 @@ n = pypsa.Network()
 # set time steps
 n.set_snapshots(pd.date_range(start="2023-01-01", end="2023-01-31", freq="H"))
 # gggg
-#hahha
+# hahha
+# lol
 
 # add a bus/region for electricity
 n.add("Bus", "windstrom")
